52784cda6184c41bae65f09f08c7075aba213079
Hayden Anderson <Hayden.Anderson@rexelusa.com>
2020-03-13 13:27:58 -0700

    added google static maps api

--- src/components/theHike/Hike.js
+++ src/components/theHike/Hike.js
@@ -2,14 +2,38 @@ import React, { Component } from "react";
 import Nav from "../Nav";
 import hikePlanData from "../../hikePlanData";
 import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import { StaticGoogleMap, Marker, Path } from "react-static-google-map";
+import { Row, Col } from "react-bootstrap";
 
 export default class Hike extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-        <div>
-          <section id="Hike">
+      <Container fluid>
+        <Row fluid>
+          <Nav />
+        </Row>
+        <Row>
+          <Col xs="3">test iasdasdfdfasdf</Col>
+          {/* <StaticGoogleMap
+              size="200x600"
+              apiKey="AIzaSyC-Q30UjGg8_-Kcc-1day5VcfrA0IExGA8"
+            >
+              <Marker.Group label="Here" color="brown">
+                <Marker location="42.065235, -122.602922" />
+              </Marker.Group>
+              <Path
+                points={[
+                  "32.5898,-116.4685",
+                  "36.6941,-118.372",
+                  "40.2132,-121.3546",
+                  "42.0038,-122.91",
+                  "45.6624,-121.8994",
+                  "49,-120.7987"
+                ]}
+              />
+            </StaticGoogleMap> */}
+          <Col xs="3">
             <Table striped bordered>
               <thead>
                 <tr>
@@ -24,34 +48,30 @@ export default class Hike extends Component {
               <tbody>
                 {hikePlanData.map((row, i) => (
                   <tr key={i}>
-                    <td className="hike-plan-item">
-                      {row["Section End Point"]}
-                    </td>
-                    <td className="hike-plan-item">
+                    <td>{row["Section End Point"]}</td>
+                    <td>
                       {row["Section Hiking Days"] === 0
                         ? ""
                         : row["Section Hiking Days"].toFixed(1)}
                     </td>
-                    <td className="hike-plan-item">
+                    <td>
                       {row["Section Distance"] === 0
                         ? ""
                         : row["Section Distance"]}
                     </td>
-                    <td className="hike-plan-item">
+                    <td>
                       {row["Average Distance/day"] === 0
                         ? ""
                         : row["Average Distance/day"].toFixed(1)}
                     </td>
-                    <td className="hike-plan-item">
-                      {formatDate(row["Arrival Date"])}
-                    </td>
+                    <td>{formatDate(row["Arrival Date"])}</td>
                   </tr>
                 ))}
               </tbody>
             </Table>
-          </section>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
